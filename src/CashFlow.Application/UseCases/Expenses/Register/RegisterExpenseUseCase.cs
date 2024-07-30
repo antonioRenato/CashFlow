01de12361a33106e69b23a358514405fb4bc040e
antonioRenato <antoniorpfs@gmail.com>
2024-07-29 21:19:26 -0300

    Creating request and response for expenses

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,13 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register
+{
+    public class RegisterExpenseUseCase
+    {
+        public ResponsesRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+        {
+            return new ResponsesRegisteredExpenseJson();
+        }
+    }
+}
