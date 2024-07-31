637e6d84f886943d6e413e5ca7be1cfbcf2e6f4e
antonioRenato <antoniorpfs@gmail.com>
2024-07-30 21:13:36 -0300

    Create validate to Expenses

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register
@@ -7,7 +8,28 @@ namespace CashFlow.Application.UseCases.Expenses.Register
     {
         public ResponsesRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
         {
+            Validate(request);
+
             return new ResponsesRegisteredExpenseJson();
+        }
+
+        private void Validate(RequestRegisterExpenseJson request)
+        {
+            var titleIsEmpty = string.IsNullOrEmpty(request.Title);
+
+            if (titleIsEmpty)
+                throw new ArgumentException("The title is required!");
+
+            if (request.Amount <= 0)
+                throw new ArgumentException("The amount must be greater than zero!");
+
+            var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+            if (result > 0)
+                throw new ArgumentException("Expenses cannot be for the future!");
+
+            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+            if (!paymentTypeIsValid)
+                throw new ArgumentException("Payment Type is not valid!");
         }
     }
 }
