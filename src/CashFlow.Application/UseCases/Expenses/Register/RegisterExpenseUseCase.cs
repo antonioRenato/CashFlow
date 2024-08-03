fbc7271b7ffb42cf14d433ffcbbf511cbea7663a
antonioRenato <antoniorpfs@gmail.com>
2024-08-03 19:42:17 -0300

    created validate with Fluent package

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -15,21 +15,11 @@ namespace CashFlow.Application.UseCases.Expenses.Register
 
         private void Validate(RequestRegisterExpenseJson request)
         {
-            var titleIsEmpty = string.IsNullOrEmpty(request.Title);
+            var validate = new RegisterExpenseValidator();
 
-            if (titleIsEmpty)
-                throw new ArgumentException("The title is required!");
+            var result = validate.Validate(request);
 
-            if (request.Amount <= 0)
-                throw new ArgumentException("The amount must be greater than zero!");
-
-            var result = DateTime.Compare(request.Date, DateTime.UtcNow);
-            if (result > 0)
-                throw new ArgumentException("Expenses cannot be for the future!");
-
-            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
-            if (!paymentTypeIsValid)
-                throw new ArgumentException("Payment Type is not valid!");
+            var errorMessages = result.Errors.Select(x => x.ErrorMessage).ToList();
         }
     }
 }
