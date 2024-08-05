b03eed3aff531c0b091f06e8607d27d06cabb48b
antonioRenato <antoniorpfs@gmail.com>
2024-08-05 20:18:39 -0300

    Created expcetions

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,7 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
-using CashFlow.Communication.Responses;
-using CashFlow.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers
@@ -13,25 +11,10 @@ namespace CashFlow.Api.Controllers
         [HttpPost]
         public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
         {
-            try
-            {
-                var useCase = new RegisterExpenseUseCase();
-                var response = useCase.Execute(request);
+            var useCase = new RegisterExpenseUseCase();
+            var response = useCase.Execute(request);
 
-                return Created(string.Empty, response);
-            }
-            catch (ErrorOnValidationException ex)
-            {
-                var errorResponse = new ResponseErrorJson(ex.Errors);
-
-                return BadRequest(errorResponse);
-            }
-            catch 
-            {
-                var errorResponse = new ResponseErrorJson("unknown error");
-
-                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
-            }
+            return Created(string.Empty, response);
         }
     }
 }
