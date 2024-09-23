8fcb9dfc807a48e6b826a60ac3e92651b84d7e37
antonioRenato <antoniorpfs@gmail.com>
2024-09-22 21:27:39 -0300

    Create entity user

--- src/CashFlow.Domain/Entities/Expense.cs
+++ src/CashFlow.Domain/Entities/Expense.cs
@@ -8,7 +8,10 @@ namespace CashFlow.Domain.Entities
         public string Title { get; set; } = string.Empty;
         public string? Description { get; set; }
         public DateTime Date {  get; set; } 
-        public decimal Amount { get; set; } 
-        public PaymentType PaymentType { get; set; }    
+        public decimal Amount { get; set; }
+        public PaymentType PaymentType { get; set; }
+
+        public long UserId { get; set; }
+        public User User { get; set; } = default!;
     }
 }
