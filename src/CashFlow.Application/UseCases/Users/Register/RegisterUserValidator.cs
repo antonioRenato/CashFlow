bac9a3636fd095db1a934ba0a848a89d751537a4
antonioRenato <antoniorpfs@gmail.com>
2024-09-24 22:04:53 -0300

    Created validation to the password

--- src/CashFlow.Application/UseCases/Users/Register/RegisterUserValidator.cs
+++ src/CashFlow.Application/UseCases/Users/Register/RegisterUserValidator.cs
@@ -10,6 +10,8 @@ namespace CashFlow.Application.UseCases.Users.Register
         {
             RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceErrorMessages.NAME_EMPTY);
             RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceErrorMessages.EMAIL_EMPTY).EmailAddress().WithMessage(ResourceErrorMessages.EMAIL_INVALID);
+
+            RuleFor(user => user.Password).SetValidator(new PasswordValidator<RequestRegisterUserJson>());
         }
     }
 }
