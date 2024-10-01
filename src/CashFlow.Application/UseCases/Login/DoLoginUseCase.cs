6c5bde9e7ae8071c5f1192ff716838fbe9701d97
antonioRenato <antoniorpfs@gmail.com>
2024-09-30 21:25:48 -0300

    Created login use case

--- src/CashFlow.Application/UseCases/Login/DoLoginUseCase.cs
+++ src/CashFlow.Application/UseCases/Login/DoLoginUseCase.cs
@@ -3,6 +3,7 @@ using CashFlow.Communication.Responses;
 using CashFlow.Domain.Repositories.User;
 using CashFlow.Domain.Security.Cryptography;
 using CashFlow.Domain.Security.Tokens;
+using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlow.Application.UseCases.Login
 {
@@ -21,9 +22,24 @@ namespace CashFlow.Application.UseCases.Login
 
         public async Task<ResponseRegisteredUserJson> Execute(RequestLoginJson request)
         {
+            var user = await _repository.GetUserByEmail(request.Email);
+
+            if (user == null)
+            {
+                throw new InvalidLoginException();
+            }
+
+            var passwordMatch = _passwordEncripter.Verify(request.Password, user.Password);
+
+            if (passwordMatch == false)
+            {
+                throw new InvalidLoginException();
+            }
+
             return new ResponseRegisteredUserJson
             {
-
+                Name = user.Name,
+                Token = _acessTokenGenerator.Generate(user), 
             };
         }
     }
---
+++ src/CashFlow.Exception/ExceptionsBase/InvalidLoginException.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace CashFlow.Exception.ExceptionsBase
+{
+    public class InvalidLoginException : CashFlowException
+    {
+        public InvalidLoginException() : base(ResourceErrorMessages.EMAIL_EMPTY)
+        {
+            
+        }
+
+        public override int StatusCode => (int)HttpStatusCode.Unauthorized;
+
+        public override List<string> GetErrors()
+        {
+            return [Message];
+        }
+    }
+}
