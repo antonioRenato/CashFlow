a629de12ccf200af83f15f30af00ca36b00f3be1
antonioRenato <antoniorpfs@gmail.com>
2024-10-02 21:55:54 -0300

    Created test for every validate to password

--- tests/Validators.Tests/Users/PasswordValidatorTest.cs
+++ tests/Validators.Tests/Users/PasswordValidatorTest.cs
@@ -1,6 +1,32 @@
-﻿namespace Validators.Tests.Users
+﻿using CashFlow.Application.UseCases.Users;
+using CashFlow.Application.UseCases.Users.Register;
+using CashFlow.Communication.Requests;
+using CommomTestUtilities.Requests;
+using FluentAssertions;
+using FluentValidation;
+
+namespace Validators.Tests.Users
 {
     public class PasswordValidatorTest
     {
+        [Theory]
+        [InlineData("")]
+        [InlineData("     ")]
+        [InlineData(null)]
+        [InlineData("a")]
+        [InlineData("aaaa")]
+        [InlineData("aaaaa")]
+        [InlineData("aaaaaaaa")]
+        [InlineData("AAAAAAAA")]
+        [InlineData("Aaaaaaaa")]
+        [InlineData("Aaaaaa11")]
+        public void Error_Email_Empty(string password)
+        {
+            var validator = new PasswordValidator<RequestRegisterUserJson>();
+
+            var result = validator.IsValid(new ValidationContext<RequestRegisterUserJson>(new RequestRegisterUserJson()), password);
+
+            result.Should().BeFalse();
+        }
     }
 }
