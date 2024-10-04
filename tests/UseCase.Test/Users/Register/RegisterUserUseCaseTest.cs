452fd7a78f97dbead2055fad6d1f920a1a365845
antonioRenato <antoniorpfs@gmail.com>
2024-10-03 22:36:05 -0300

    Create tests to test usecase for expense and user

---
+++ tests/UseCase.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,29 @@
+﻿using CashFlow.Application.UseCases.Users.Register;
+using CommomTestUtilities.Requests;
+using FluentAssertions;
+
+namespace UseCase.Test.Users.Register
+{
+    public class RegisterUserUseCaseTest
+    {
+        [Fact]
+        public async Task Sucess()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            var useCase = CreateUseCase();
+
+            var result = await useCase.Execute(request);
+
+            result.Should().NotBeNull();
+            result.Name.Should().Be(request.Name);
+            result.Token.Should().NotBeNullOrWhiteSpace();
+        }
+
+        private RegisterUserUseCase CreateUseCase()
+        {
+            return new RegisterUserUseCase();
+        }
+    }
+
+
+}
