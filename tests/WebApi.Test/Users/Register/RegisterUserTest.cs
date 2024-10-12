af1f1892d38e39b884c85846a4e498dbd9131a86
antonioRenato <antoniorpfs@gmail.com>
2024-10-12 17:00:59 -0300

    Created test to verify name and token

--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace WebApi.Test.Users.Register
 {
@@ -25,6 +26,13 @@ namespace WebApi.Test.Users.Register
             var result = await _httpClient.PostAsJsonAsync(METHOD, request);
 
             result.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            var body = await result.Content.ReadAsStreamAsync();
+
+            var response = await JsonDocument.ParseAsync(body);
+
+            response.RootElement.GetProperty("name").GetString().Should().Be(request.Name);
+            response.RootElement.GetProperty("token").GetString().Should().NotBeNullOrEmpty();
         }
     }
 }
