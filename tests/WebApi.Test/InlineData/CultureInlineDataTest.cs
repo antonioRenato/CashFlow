c6a9256de499b3a7e4ff5e562e31ddf0f775583e
antonioRenato <antoniorpfs@gmail.com>
2024-10-13 14:14:05 -0300

    Create test for different languages

---
+++ tests/WebApi.Test/InlineData/CultureInlineDataTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace WebApi.Test.InlineData
+{
+    public class CultureInlineDataTest : IEnumerable<Object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new Object[] { "fr" };
+            yield return new Object[] { "pt-BR" };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
