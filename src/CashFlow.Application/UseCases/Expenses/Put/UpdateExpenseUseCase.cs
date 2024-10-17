2be6772224463ea94d4c4720322c1112a06ccf02
antonioRenato <antoniorpfs@gmail.com>
2024-10-16 22:21:17 -0300

    Verifing when post or updating the expense if the user ir logged

--- src/CashFlow.Application/UseCases/Expenses/Put/UpdateExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Put/UpdateExpenseUseCase.cs
@@ -2,6 +2,7 @@
 using CashFlow.Communication.Requests;
 using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Domain.Services.LoggedUser;
 using CashFlow.Exception;
 using CashFlow.Exception.ExceptionsBase;
 
@@ -12,21 +13,25 @@ namespace CashFlow.Application.UseCases.Expenses.Put
         private readonly IMapper _mapper;
         private readonly IUnitOfWork _unitOfWork;
         private readonly IExpensesUpdateOnlyRepository _repository;
+        private readonly ILoggedUser _loggedUser;
         
-        public UpdateExpenseUseCase(IMapper mapper, IUnitOfWork unitOfWork, IExpensesUpdateOnlyRepository repository)
+        public UpdateExpenseUseCase(IMapper mapper, IUnitOfWork unitOfWork, IExpensesUpdateOnlyRepository repository, ILoggedUser loggedUser)
         {
             _mapper = mapper;
             _unitOfWork = unitOfWork;   
             _repository = repository;
+            _loggedUser = loggedUser;
         }
 
         public async Task Execute(long id, RequestExpenseJson request)
         {
             Validate(request);
 
+            var loggedUser = await _loggedUser.Get();
+
             var expense = await _repository.Get(id);
 
-            if (expense == null) 
+            if (expense == null || expense.UserId != loggedUser.Id) 
             {
                 throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
             }
