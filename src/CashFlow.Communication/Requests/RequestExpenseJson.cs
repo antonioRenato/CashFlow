48002c2496d6ee6a480c1db559924880d5d671f6
antonioRenato <antoniorpfs@gmail.com>
2024-10-31 22:11:25 -0300

    Creasted auto mapping to tags request

--- src/CashFlow.Communication/Requests/RequestExpenseJson.cs
+++ src/CashFlow.Communication/Requests/RequestExpenseJson.cs
@@ -8,6 +8,7 @@ namespace CashFlow.Communication.Requests
         public string Description { get; set; } = string.Empty;
         public DateTime Date { get; set; }
         public decimal Amount { get; set; }
-        public PaymentType PaymentType { get; set; }    
+        public PaymentType PaymentType { get; set; }
+        public IList<Tag> Tags { get; set; } = [];
     }
 }
